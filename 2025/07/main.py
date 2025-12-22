384eff04d797151ca611ed8f30541577e98c1a71
Adam Havlíček <50631920+AdamHavlicek@users.noreply.github.com>
2025-12-22 12:38:35 +0100

    feat: solved problem 07 first part

---
+++ 2025/07/main.py
@@ -0,0 +1,50 @@
+import os
+import pprint
+
+
+def read_lines(path):
+    with open(path, 'r') as opened_file:
+        return opened_file.read()
+
+def parse_lines(lines):
+    return [list(line) for line in lines.splitlines()]
+
+def process_lines(lines: [[str]]):
+    split_counter = 0
+    allowed_characters = ['|', 'S']
+
+    for line_index, line in enumerate(lines, 0):
+        for char_index, char in enumerate(line):
+            is_line_above = line_index - 1 >= 0 and lines[line_index - 1][ char_index] in allowed_characters
+            if char == "^" and is_line_above:
+                lines[line_index][char_index - 1] = "|"
+                lines[line_index][char_index + 1] = "|"
+
+                split_counter += 1
+                continue
+
+            if is_line_above:
+                lines[line_index][char_index] = "|"
+
+
+    return split_counter
+
+def display_lines(lines: [[str]]):
+    pprint.pprint(["".join(line) for line in lines])
+
+def main():
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        # 'input.txt',
+        'input_real.txt',
+    )
+
+    lines = read_lines(file_path)
+    lines = parse_lines(lines)
+
+    print(process_lines(lines))
+    display_lines(lines)
+
+if __name__ == '__main__':
+    main()
+    exit(0)
